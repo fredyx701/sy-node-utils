b1bb653d60edb16e2421159769cac085e3052f44
fredy <467941026@qq.com>
2019-07-30 14:34:19 +0800

    [update]  add  if logic

--- tools/render.js
+++ tools/render.js
@@ -9,6 +9,8 @@ const else_reg = /\{\{else\}\}/;
 const end_if_reg = /\{\{\s*\/if\s*\}\}/; // 加上空格, 全文判断
 const condition_reg = /\{\{(if\s+(?:.*?)|else|else\s+if(?:.*?)|\/if)\}\}/g;
 
+const logic_reg = /===|!==|!|\|\||&&|>|</;
+const string_reg = /"(.*?)"/;
 
 function render(str, data) {
     if (end_if_reg.test(str)) {
@@ -23,7 +25,7 @@ function render(str, data) {
 
 function ifrender(str, data) {
     str = str.replace(variable_reg, function(match, p1) {
-        p1 = p1.trim();
+        p1 = p1.trim(); // if表达式去除前后空格
         if (p1.includes('if ') || p1.includes('else') || p1.includes('/if')) {
             return `{{${p1}}}`;
         }
@@ -58,7 +60,7 @@ function ifrender(str, data) {
 
         if (if_reg.test(match_str)) {
             info.type = 1;
-            info.value = Boolean(_.get(data, if_reg.exec(match_str)[1], false));
+            info.value = renderExpression(if_reg.exec(match_str)[1], data);
 
             const i = current.length - 1;
             const child = [ info ];
@@ -72,7 +74,7 @@ function ifrender(str, data) {
 
         } else if (else_if_reg.test(match_str)) {
             info.type = 2;
-            info.value = Boolean(_.get(data, else_if_reg.exec(match_str)[1], false));
+            info.value = renderExpression(else_if_reg.exec(match_str)[1], data);
             current.push(info);
         } else if (else_reg.test(match_str)) {
             info.type = 3;
@@ -112,5 +114,78 @@ function spell(current) {
     }
     return result;
 }
+
+
+function renderExpression(str, data) {
+    const match = logic_reg.exec(str);
+    let result = false;
+    let val1,
+        val2;
+    if (match) {
+        const logic_symbol = match[0];
+        switch (logic_symbol) {
+        case '===':
+            [ val1, val2 ] = getVal(str, data, '===');
+            result = val1 === val2;
+            break;
+        case '!==':
+            [ val1, val2 ] = getVal(str, data, '!==');
+            result = val1 !== val2;
+            break;
+        case '!':
+            [ val1 ] = getVal(str, data, '!');
+            result = !val1;
+            break;
+        case '>':
+            [ val1, val2 ] = getVal(str, data, '>');
+            result = val1 > val2;
+            break;
+        case '<':
+            [ val1, val2 ] = getVal(str, data, '<');
+            result = val1 < val2;
+            break;
+        case '||':
+            [ val1, val2 ] = getVal(str, data, '||');
+            result = val1 || val2;
+            break;
+        case '&&':
+            [ val1, val2 ] = getVal(str, data, '&&');
+            result = val1 && val2;
+            break;
+        default:
+            break;
+        }
+    } else {
+        result = _.get(data, str, false);
+    }
+
+    return Boolean(result);
+}
+
+
+function getVal(str, data, logic_symbol) {
+    const result = [];
+    let vals = [];
+    if (logic_symbol === '!') {
+        str = str.replace('!', '');
+        vals.push(str);
+    } else {
+        vals = str.split(logic_symbol);
+    }
+    for (let val of vals) {
+        val = val.trim();
+        const match = string_reg.exec(val);
+        if (match) {
+            val = match[1];
+        } else if (!isNaN(+val)) {
+            val = +val;
+        } else {
+            val = _.get(data, val, '');
+        }
+        result.push(val);
+    }
+    return result;
+}
+
 
 module.exports = render;
